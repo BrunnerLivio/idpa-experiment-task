0f480aa925256150635154a6c220a6cf826c1f8e
Livio Brunner <livio.brunner.lb1@gmail.com>
2017-12-19 14:25:46 +0100

    add test solution

--- README.md
+++ README.md
@@ -1,3 +1,43 @@
 # idpa-experiment-task
 
-## Task
+## Aufgabe
+
+Schreiben Sie ein Programm, das die Zahlen von 1 bis 100 ausgibt. Bei jeder Zahl, die durch 5 teilbar ist, soll "Fizz" ausgegeben werden und bei jeder Zahl, die durch 7 teilbar ist, soll "Buzz" ausgegeben werden. Wenn die Zahl sowohl durch 7 als auch durch 5 teilbar ist, soll "FizzBuzz" ausgegeben werden.
+
+### Beispielausgabe
+
+```
+1 2 Fizz 4 Buzz Fizz 7 8 Fizz Buzz 11 Fizz 13 14 FizzBuzz 16 [...] 98 Fizz Buzz
+```
+
+## Ausführen
+
+Drücke F5 in VSCode oder führe im Terminal aus:
+
+```bash
+cd idpa-experiment-task/idpa-experiment-task
+dotnet restore
+dotnet run
+```
+
+## Tests
+
+Führe im Terminal aus:
+
+```bash
+cd idpa-experiment-task/idpa-experiment-task.Tests
+dotnet restore
+dotnet test
+```
+
+
+## Checkliste Aktuelles Wissen
+
+Bei den folgenden Checkboxen sollen Sie ankreuzen, welche Themen Sie verstehen und anwenden können in der Programmiersprache C#. Diese Themen können für den Auftrag relevant oder irrelevant sein.
+
+- [ ] if-Bedingungen
+- [ ] for/while-Schleifen
+- [ ] Operatoren +, -, /, *, %
+- [ ] Funktion mit Parameter und Rückgabewerte
+- [ ] Array / Listen
+- [ ] Datentypen Boolean, Integer, String 
---
+++ idpa-experiment-task/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace idpa_experiment_task
+{
+    public class FizzBuzz
+    {
+        /// <summary>
+        /// Gibt die Zahlen vo 1 bis 100 zurück als string. 
+        /// Allerdings, bei jeder Zahl, die durch 5 teilbar ist, wird "Fizz" ausgegeben und 
+        /// bei jeder Zahl, die durch 7 teilbar ist, wird "Buzz" ausgegeben. 
+        /// Wenn die Zahl sowohl durch 7 als auch durch 5 teilbar ist, wird "FizzBuzz" ausgegeben.
+        /// </summary>
+        /// <param name="iterations">Die Anzahl Nummern es ausgeben sollte</param>
+        /// <example>
+        /// > FizzBuzz.Print(100);
+        /// 1 2 Fizz 4 Buzz Fizz 7 8 Fizz Buzz 11 Fizz 13 14 FizzBuzz 16 [...] 98 Fizz Buzz
+        /// </example>
+        public string GetFizzBuzzString(int iterations)
+        {
+            string fizzBuzzString = "";
+            for (int i = 1; i <= iterations; i++)
+            {
+                bool fizz = i % 3 == 0;
+                bool buzz = i % 5 == 0;
+                if (fizz && buzz)
+                    fizzBuzzString += "FizzBuzz ";
+                else if (fizz)
+                    fizzBuzzString += "Fizz ";
+                else if (buzz)
+                    fizzBuzzString += "Buzz ";
+                else
+                    fizzBuzzString += i + " ";
+            }
+            return fizzBuzzString;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FizzBuzz fizz = new FizzBuzz();
+            Console.WriteLine(fizz.GetFizzBuzzString(231));
+        }
+    }
+}
